2ae1c8992ca279c868e991519a78643b766a7c7e
Raven <sd5356@rit.edu>
2025-01-27 14:42:54 -0500

    Add: a more helpful help page.

--- frontend/src/app/how_to_play.rs
+++ frontend/src/app/how_to_play.rs
@@ -7,9 +7,12 @@ pub fn how_to_play() -> Html {
     let onclick = Callback::from(move |_| navigator.back());
 
     html! {
-        <div class="flex flex-col mx-auto mt-12 max-w-md text-center gap-3 items-center">
-            <h1>{ "Fucking Google it you dumbass" }</h1>
-            <p>{ "Or watch this " } <a class="text-indigo-500" href="https://www.youtube.com/watch?v=_Na3a1ZrX7c">{ "video" }</a> { " by Vsauce." }</p>
+        <div class="flex flex-col mx-auto mt-12 max-w-md gap-3 items-center">
+            <h1 class="text-2xl font-bold">{ "Rules" }</h1>
+            <p>{ "Just like in regular tic-tac-toe, the two players (X and O) take turns, starting with X. The game starts with X playing wherever they want in any of the 81 empty spots. Thereafter, each player moves in the small board corresponding to the position of the previous move in its small board, as indicated in the figures." }</p>
+            <p> { "If a move is played so that it wins a small board by the rules of normal tic-tac-toe, then the entire small board is marked as won by the player in the larger board. Once a small board is won by a player or it is filled completely, no more moves may be played in that board. If a player is sent to such a board, then that player may play in any other board. Game play ends when either a player wins the larger board or there are no legal moves remaining, in which case the game is a draw." }</p>
+            <p class="text-center ">{ "Read more on "}<a class="text-secondary" href="https://en.wikipedia.org/wiki/Ultimate_tic-tac-toe">{ "Wikipedia" }</a></p>
+            <p class="text-center ">{ "Or watch this " } <a class="text-secondary" href="https://www.youtube.com/watch?v=_Na3a1ZrX7c">{ "video" }</a> { " by Vsauce." }</p>
             <button class="font-semibold text-sm bg-primary rounded-full shadow-sm px-4 py-2 max-w-fit" onclick={onclick}>{"Back"}</button>
         </div>
     }
